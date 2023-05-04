783c87b80270e3dc4b2a337e27bdf3c5b11c92ca
Simonov Artem <DragonSteve304@gmail.com>
2023-05-04 22:24:53 +0300

    Okay initializer works

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿using Vector;
 
-Vector<int> arr = new Vector<int>();
+Vector<int> arr = new Vector<int>(new int[] {1, 2, 3});
 
-arr.PushBack(1);
 
-Console.WriteLine(arr[0]);
+
+for (int i = 0; i < arr.Size(); i++)
+     Console.WriteLine(arr[i]);
+
